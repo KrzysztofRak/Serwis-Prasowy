a5dd0bb3982cbf4f50edf2a64870c2b24ae2ff02
Administrator <Administrator@WIN-SHAREPOINTD>
2018-01-26 11:15:10 +0100

    Add project files.

---
+++ SerwisPrasowy/WebParts/Latest News/Latest News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+
+namespace SerwisPrasowy.WebParts.Latest_News
+{
+    [ToolboxItemAttribute(false)]
+    public class Latest_News : WebPart
+    {
+        protected override void CreateChildControls()
+        {
+        }
+    }
+}
